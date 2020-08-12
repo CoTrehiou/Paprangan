eb744cccdb8f2d43f9c271d2e36a4b89eaf7086b
Papouboy <corentin.trehiou@wanadoo.fr>
2020-08-12 10:52:17 +0200

    [ADD] Heros movement

--- Assets/DEV/Heros/Scripts/Heros.cs
+++ Assets/DEV/Heros/Scripts/Heros.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Heros : MonoBehaviour
 {
+
+    [Header("KeyCode")]
+    [SerializeField] KeyCode _forwardKey = KeyCode.Z;
+    [SerializeField] KeyCode _backwardKey = KeyCode.S;
+    [SerializeField] KeyCode _leftKey = KeyCode.Q;
+    [SerializeField] KeyCode _rightKey = KeyCode.D;
+
+    [SerializeField] Transform _herosTransform;
+    [SerializeField] float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move(_forwardKey, Vector3.forward);
+        Move(_backwardKey, Vector3.back);
+        Move(_leftKey, Vector3.left);
+        Move(_rightKey, Vector3.right);
+    }
+
+    private void Move(KeyCode key, Vector3 direction)
+    {
+
+        if (Input.GetKey(key))
+        {
+            _herosTransform.Translate(direction * Time.deltaTime * speed);
+        }
     }
 }
